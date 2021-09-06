4c5b0fbc56f837f7d84ae04ae35451c4e82298aa
Conor <c17730535@mydit.ie>
2021-09-06 17:20:44 +0100

    Add basic project structure to get React Working

---
+++ src/App.js
@@ -0,0 +1,14 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Update Component</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));

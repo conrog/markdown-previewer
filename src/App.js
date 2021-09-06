befb6937c4445c20676238eb4649fea21d712814
Conor <c17730535@mydit.ie>
2021-09-06 18:32:55 +0100

    Added two way binding between Editor and Preview Component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      text: "placeholder text...",
+    };
+  }
+  handleChange(event) {
+    this.setState({ text: event.target.value });
   }
   render() {
     return (
       <div>
-        <Editor />
-        <Preview />
+        <Editor text={this.state.text} onChange={this.handleChange} />
+        <Preview text={this.state.text} />
       </div>
     );
   }
 }
 
-const Editor = () => {
+const Editor = (props) => {
   return (
     <div>
-      <p>Editor</p>
-      <textarea></textarea>
+      <p>Editor: </p>
+      <textarea value={props.text} onChange={props.onChange}></textarea>
     </div>
   );
 };
 
-const Preview = () => {
+const Preview = (props) => {
   return (
     <div>
-      <p>Preview</p>
+      <p>Preview: </p>
+      <p>{props.text}</p>
     </div>
   );
 };
